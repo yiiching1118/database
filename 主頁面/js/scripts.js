70ea341b9b4b92955d75cee2e1576a7582ff478b
r11725016 <r11725016@ntu.edu.tw>
2024-05-15 10:34:33 +0800

    first commit

---
+++ 主頁面/js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// 當頁面滾動時，顯示或隱藏返回頂部按鈕
+
+
